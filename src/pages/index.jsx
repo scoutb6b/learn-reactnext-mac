39631f708bfb88015802cb29728c57b7d00ba4d5
scoutb6b <scoutboq@gmail.com>
2024-01-23 23:09:32 +0900

    ⚗️  クリックイベントの実験　不要につきそのうち削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,28 @@ import { Inter } from "next/font/google";
 import { Links } from "src/components/Links";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const foo = 1;
+
+  const handeleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <Header />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <a href="/about" onClick={handeleClick}>
+          ボタン
+        </a>
         <Main pages="index!!" />
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
