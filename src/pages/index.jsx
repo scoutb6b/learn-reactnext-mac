e4114c78e6f041b87cc8e50ecfe7535cc76da34a
scoutb6b <scoutboq@gmail.com>
2024-01-24 21:40:46 +0900

    ♻️ aタグだとvercelにデプロイ時にエラーが出たので、Linkに修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import { Links } from "src/components/Links";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { useCallback, useEffect } from "react";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,9 +32,9 @@ export default function Home() {
     >
       <Header />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <a href="/about" onClick={handeleClick}>
+        <Link href="/about" onClick={handeleClick}>
           ボタン
-        </a>
+        </Link>
         <Main pages="index!!" />
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
