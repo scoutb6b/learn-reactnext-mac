71f4e74a4a9f190c83aee2aee313c7e33306cd42
scoutb6b <scoutboq@gmail.com>
2024-03-12 23:23:42 +0900

    ✨ useRouter導入

--- src/hooks/useBgColor.jsx
+++ src/hooks/useBgColor.jsx
@@ -1,10 +1,29 @@
-import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useMemo } from "react";
 
 export const useBgColor = () => {
+  const router = useRouter();
+
+  const bgColor = useMemo(() => {
+    // return router.pathname === "/" ? "lightblue" : "lightpink";
+    // swithch文で条件分岐してもあり
+    switch (router.pathname) {
+      case "/": {
+        return "lightblue";
+      }
+      case "/about": {
+        return "lightpink";
+      }
+      default: {
+        return "";
+      }
+    }
+  }, [router.pathname]);
+
   useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
+    document.body.style.backgroundColor = bgColor;
     return () => {
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [bgColor]);
 };
